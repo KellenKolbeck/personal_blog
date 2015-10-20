275a4a74a912b69a95d5e865a0c9db51397afe8c
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-10-19 18:18:56 -0700

    Add crud functionality for posts and tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,51 @@
-class ListsController < ApplicationController
+class PostsController < ApplicationController
   def index
-    @posts = List.all
-    render :index
+      @posts = Post.all
+      @tags = Tag.all
+    end
+
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to posts_path
+      else
+        render :new
+      end
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+    def update
+      @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to posts_path
+    end
+
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
+
+
 end
