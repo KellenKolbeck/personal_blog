14523f6f2d32c17b825a735f8f6757d1a58936f4
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-10-23 01:36:26 -0700

    Minor changes to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(:email 'randy@thecat.com', :password 'pdx123pdx456pdx789', admin: true)
+User.create(email: 'randy@thecat.com', password: 'pdx123pdx456pdx789', admin: true)
